9014d3b34b325d3b6c820126edbc367c5d6fc446
miguel <guegosilvestre67@gmail.com>
2020-07-27 17:58:27 -0300

    Class

--- main.js
+++ main.js
@@ -1,26 +1,7 @@
-class List{
-    constructor(){
-        this.data = [];
-    }
-    add(data){
-        this.data.push(data);
-        console.log(this.data);
-    }
-}
-class TodoList extends List{
-    constructor(){
-        super();
-        this.usuario = "Diego";
-    }
-    mostraUsuario(){
-        console.log(this.usuario);
+class Matematica{
+    static soma(a, b){
+        return a + b;
     }
 }
 
-var MinhaLista = new TodoList();
-
-document.getElementById('novoTodo').onclick = function(){
-    MinhaLista.add('Novo Todo');
-}
-
-MinhaLista.mostraUsuario();
+console.log(Matematica.soma(1 , 2));
