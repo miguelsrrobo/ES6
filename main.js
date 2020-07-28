123479e1ad129a341fced871215246be88088ba5
miguel <guegosilvestre67@gmail.com>
2020-07-28 10:20:00 -0300

    Operações em Array

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-function teste(x){
-    let y = 2;
- 
-    if(x > 5){
-        let y = 4;
-        console.log(x, y);
-    }
-}
+const arr = [1, 3, 4, 5, 8, 9];
 
-teste(10);
+const newArr = arr.map(function(item, index){
+    return item * 2;
+});
+
+console.log(newArr);
+
+const sum = arr.reduce(function(total, next){
+    return total + next;
+});
+
+console.log(sum);
+
+const filter = arr.filter(function(item){
+    return item % 2 == 0;
+});
+
+console.log(filter);
+
+const find = arr.find(function(item){
+    return item === 2 ;
+});
+
+console.log(find);
