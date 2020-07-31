b9e2905163ff36bed0ddaf9c0bfe5864d004d312
miguel <guegosilvstre67@gmail.com>
2020-07-30 21:01:10 -0300

    Operadores rest/spread

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
  // REST
 
-function soma(...params){
-  return params.reduce((total, next) => total + next);
+function soma(a, b, ...params){
+  return params;
 }
 
-console.log(soma(1,3,4));
+console.log(soma(1,3,4,5,6,7,8,9));
