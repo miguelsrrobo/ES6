31f860f1d1048141468578c321d0b29ea1fe042a
miguel <guegosilvstre67@gmail.com>
2020-07-30 21:08:30 -0300

    Operadores rest/spread

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ console.log(soma(1,3,4,5,6,7,8,9));
 
  // SPREAD
 
-const arr1 = [1,2,3];
-const arr2 = [4,5,6];
+const usuario1 = {
+  nome: 'Diego',
+  idade: 23,
+  empresa: 'Rocketseat',
+};
 
-const arr3 = [...arr1, ...arr2];
-
-console.log(arr3);
+const usuario2 = {... usuario1, nome: 'Gabriel'};
+console.log(usuario2);
